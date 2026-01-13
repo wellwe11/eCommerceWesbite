140a941ff9cb4125421584a718941d6630feef55
Robin Ryan <robin1ryan@hotmail.com>
2026-01-13 16:24:36 +0200

    updated some layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,10 @@
-import { ReactLenis, useLenis } from "lenis/react";
+import { ReactLenis } from "lenis/react";
 
 import "./App.css";
 
 import FramerMotion from "../components/FramerMotion/index.jsx";
 
 function App() {
-  const lenis = useLenis((lenis) => {
-    console.log(lenis);
-  });
   return (
     <div>
       <ReactLenis />
