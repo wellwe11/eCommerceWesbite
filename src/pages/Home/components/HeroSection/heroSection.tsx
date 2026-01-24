99e3a0564c4f331e841ef63789a488ec2c25d5e5
Robin Ryan <robin1ryan@hotmail.com>
2026-01-24 10:19:00 +0200

    updated heroSections transitions to not be so visible when scrolling into next sections texts that appear

--- src/pages/Home/components/HeroSection/heroSection.tsx
+++ src/pages/Home/components/HeroSection/heroSection.tsx
@@ -5,7 +5,7 @@ import { motion, useScroll, useTransform } from "framer-motion";
 
 const SideText = () => {
   return (
-    <div className="relative -bottom-15 p-10 z-10 self-center col-start-1 row-start-1 justify-self-end [writing-mode:vertical-rl] flex items-center gap-5">
+    <div className="p-10 z-10 [writing-mode:vertical-rl] flex items-center gap-5">
       <p className="text-xs">Small uninformative text, minor details</p>
       <h4 className="text-5xl">Big title text</h4>
     </div>
@@ -14,7 +14,7 @@ const SideText = () => {
 
 const HeroText = () => {
   return (
-    <div className="relative self-end p-10 z-10 col-start-1 row-start-1 justify-self-start">
+    <div className="p-10 z-10 justify-self-start">
       <h1 className="text-[175px] font-light leading-50">
         Art,
         <br /> for you
@@ -36,10 +36,23 @@ const HeroSection = () => {
     offset: ["start end", "end start"],
   });
 
-  const opacity = useTransform(scrollYProgress, [0, 0.2, 0.6, 1], [0, 1, 1, 0]);
-  const containerY = useTransform(
+  const opacity = useTransform(
     scrollYProgress,
-    [0, 0.2, 0.5, 1],
+    [0, 0.2, 0.6, 0.7],
+    [0, 1, 1, 0],
+  );
+
+  // SideText
+  const containerYOne = useTransform(
+    scrollYProgress,
+    [0, 0.2, 0.3, 0.7],
+    [50, 0, 0, 50],
+  );
+
+  // HeroText
+  const containerYTwo = useTransform(
+    scrollYProgress,
+    [0, 0.2, 0.4, 0.7],
     [50, 0, 0, 50],
   );
 
@@ -48,10 +61,20 @@ const HeroSection = () => {
       <motion.div
         ref={containerRef}
         className="fixed w-full h-full grid grid-cols-1"
-        style={{ opacity, y: containerY }}
+        style={{ opacity }}
       >
-        <SideText />
-        <HeroText />
+        <motion.div
+          style={{ y: containerYOne }}
+          className="relative -bottom-15 self-center justify-self-end col-start-1 row-start-1"
+        >
+          <SideText />
+        </motion.div>
+        <motion.div
+          style={{ y: containerYTwo }}
+          className="relative self-end col-start-1 row-start-1"
+        >
+          <HeroText />
+        </motion.div>
       </motion.div>
       <button
         onClick={handleNavigate}
