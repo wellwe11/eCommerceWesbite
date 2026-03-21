fe1b91db3675c36fe96d2cfb2157947071e52b06
Robin Ryan <robin1ryan@hotmail.com>
2026-03-21 12:04:18 +0200

    feat(product): Images and bio is now added. Current implemintation is clean and works well. It simply fetches specific object from the gallery array. It then passes that data as a prop. The images are then rendered using flex-wrap which creates a clean style which is similar to the gallery style, and to me feels similar. So it becomes easy to navigate

--- src/pages/Product/product.tsx
+++ src/pages/Product/product.tsx
@@ -10,31 +10,81 @@ import useProductData from "@/hooks/useProductData";
 // 2nd grid container should have information. So thats price, dimensions, colors etc.
 // All other grid containers should be the images of the product.
 
-// Basic grid
-const GridLayout = () => {};
-
 // Sorter that places bio into 2nd grid-container
-const GridSetup = () => {};
+const GridSetup = ({ data }) => {
+  console.log(data);
+
+  const images = data.images;
+  const imageOne = images[0];
+
+  const { colors, height, width, price, name } = data;
+  const bio = [colors, height, width, price, name];
+  const restImages = images.slice(1);
+
+  console.log(imageOne, restImages);
+
+  // Store imageOne in one container with class flexWrap
+  // Store bio in another container with class flexWrap
+  // Store restImages inside of a map, with each item having class flexWrap
+
+  const flexWrap = "w-full h-full overflow-hidden";
+
+  return (
+    <div className="flex w-full flex-wrap justify-center items-center gap-y-7">
+      <div className="grid grid-cols-[repeat(2,clamp(100px,30vw,400px))] pt-30 justify-center justify-items-center w-full gap-x-10">
+        <div className={flexWrap}>
+          <img
+            src={imageOne.src}
+            alt=""
+            className="object-center w-[clamp(100px,30vw,400px)] h-full"
+          />
+        </div>
+        <div className={flexWrap}>
+          {bio.map((item) => (
+            <p>{item}</p>
+          ))}
+        </div>
+      </div>
+
+      <div className="grid grid-cols-[repeat(2,clamp(100px,30vw,400px))] justify-center justify-items-center w-full gap-y-7 gap-x-10">
+        {restImages.map((image, i) => (
+          <div className={flexWrap} key={i}>
+            <img
+              src={image.src}
+              alt=""
+              className="object-center w-full h-full"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const Product = () => {
   const setAtom = useSetAtom(activeProductAtom);
   const { id } = useParams();
+
+  // Fetches placeholder data and updated data, caches it as 'product'
   const { product, isFetchingNull } = useProductData(id);
 
   // Cleans up atom on unmount
   useEffect(() => {
-    console.log("Cleaning up Product page. Setting activeProductAtom to null");
-    return () => setAtom(null);
+    return () => {
+      console.log(
+        "Cleaning up Product page. Setting activeProductAtom to null",
+      );
+      setAtom(null);
+    };
   }, [setAtom]);
 
   if (!product) return <div>Loading...</div>;
 
-  console.log(product);
-
   return (
-    <div>
+    <main>
       <h1>This is the product page of product {product.id}</h1>
-    </div>
+      <GridSetup data={product} />
+    </main>
   );
 };
 
