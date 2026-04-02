3a9ef3e8824e978774ae5488f0b614c2b3fc663f
Robin Ryan <robin1ryan@hotmail.com>
2026-04-02 13:41:57 +0300

    updated nav-tests to test logo as well

--- src/components/layout/Navbar/logo.tsx
+++ src/components/layout/Navbar/logo.tsx
@@ -4,6 +4,7 @@ const Logo = () => {
   return (
     <Link
       to="/"
+      aria-label="Home logo"
       className="flex items-center justify-center cursor-pointer pointer-events-auto"
     >
       <h1 className="text-5xl">Art & Co.</h1>
--- src/components/layout/Navbar/navbar.test.tsx
+++ src/components/layout/Navbar/navbar.test.tsx
@@ -1,8 +1,9 @@
-import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { MemoryRouter, Routes, Route } from "react-router-dom";
 import { expect, it, describe } from "vitest";
 import Navbar from "./navbar";
 
+// Test that navbar have appropriate links
 describe("Navbar Navigation Links", () => {
   it("should have the correct paths for all navigation buttons", () => {
     render(
@@ -11,12 +12,43 @@ describe("Navbar Navigation Links", () => {
       </MemoryRouter>,
     );
 
-    const homeLink = screen.getByRole("link", { name: /home/i });
+    const homeLink = screen.getByRole("link", { name: /^home$/i });
+    const logoLink = screen.getByRole("link", { name: /home logo/i });
     const galleryLink = screen.getByRole("link", { name: /gallery/i });
     const contactLink = screen.getByRole("link", { name: /contact/i });
 
     expect(homeLink).toHaveAttribute("href", "/");
+    expect(logoLink).toHaveAttribute("href", "/");
     expect(galleryLink).toHaveAttribute("href", "/gallery");
     expect(contactLink).toHaveAttribute("href", "/contact");
+  });
+});
+
+// Test that links actually are there & work
+describe("Navbar Integration", () => {
+  it("should navigate to the gallery page when the link is clicked", () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<div>Home page</div>} />
+          <Route path="/gallery" element={<div>Gallery page</div>} />
+        </Routes>
+      </MemoryRouter>,
+    );
+
+    const galleryLink = screen.getByRole("link", { name: /gallery/i });
+    const logoLink = screen.getByRole("link", { name: /home logo/i });
+    const homeLink = screen.getByRole("link", { name: /^home$/i });
+
+    fireEvent.click(galleryLink);
+    expect(screen.getByText(/gallery page/i)).toBeDefined();
+
+    fireEvent.click(logoLink);
+    expect(screen.getByText(/home page/i)).toBeDefined();
+
+    fireEvent.click(homeLink);
+    expect(screen.getByText(/home page/i)).toBeDefined();
   });
 });
