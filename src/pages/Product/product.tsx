1c93ad263fdb9bbd2634f02625a0334e258b1645
Robin Ryan <robin1ryan@hotmail.com>
2026-04-25 08:23:43 +0300

    cleaned up the states of displaying grid/hiding grid, mouse etc. Now it's working smoothly. Need to test it further for bugs but otherwise it seems to be working fine.

--- src/pages/Product/product.tsx
+++ src/pages/Product/product.tsx
@@ -62,7 +62,6 @@ const DirectionContainers = () => {
 const CenteredText = () => {
   const [displayCursor, handleCursor] = useAtom(handleCustomCursor);
   const activeIndex = useAtomValue(handleActiveArtistAtom);
-  const setCount = useSetAtom(setCountAtom);
   const artArray = useAtomValue(handleArtAtom);
   const activeArt = useAtomValue(activeArtObj);
   const activeArtist = useAtomValue(handleArtistAtom);
@@ -74,138 +73,86 @@ const CenteredText = () => {
   const { name: artistName } = activeArtist;
 
   return (
-    <div
-      className="bio-title absolute top-0 inset-x-0 h-full w-full flex justify-center items-center pointer-events-none"
-      onMouseLeave={() => {
-        setDisplayGrid(false);
-        handleCursor(true);
-      }}
-    >
-      {!displayGrid && (
-        <div className="flex justify-between w-full px-10 p-5">
-          <div
-            className="flex gap-10 pointer-events-auto"
-            onMouseMove={() => {
-              if (displayCursor) {
-                setDisplayGrid(true);
-              }
-            }}
-            onMouseLeave={() => handleCursor(true)}
-            onMouseEnter={() => handleCursor(false)}
-          >
-            <p
-              className={`mix-blend-difference text-white pointer-events-none transition-opacity duration-[400ms] ease-in-out flex gap-5 ${
-                displayCursor ? "opacity-100" : "opacity-0"
-              }`}
-            >
-              <span>
-                Issue <span className="italic">{artName}</span>
-              </span>
-              <span>by {artistName}</span>
-              <span>{year}</span>
-            </p>
-          </div>
-
-          <div
-            className="pointer-events-auto"
-            onMouseMove={() => {
-              if (displayCursor) {
-                setDisplayGrid(true);
-              }
-            }}
-            onMouseLeave={() => handleCursor(true)}
-            onMouseEnter={() => handleCursor(false)}
-          >
-            <p
-              className={`mix-blend-difference text-white pointer-events-none transition-opacity duration-[400ms] ease-in-out flex gap-5 ${
-                displayCursor ? "opacity-100" : "opacity-0"
-              }`}
-            >
-              <span>GRID</span>
-              <span>
-                {artArray.length < 10 ? 0 : ""}
-                {activeIndex + 1} - 0{artArray.length}
-              </span>
-            </p>
-          </div>
-        </div>
-      )}
-
-      {displayGrid && (
-        <div
-          className="flex justify-between w-full px-10 pointer-events-auto"
-          onMouseLeave={() => {
-            handleCursor(true);
-            setDisplayGrid(false);
-          }}
-        >
-          {artArray.map((obj, index) => (
-            <div
-              key={index}
-              className="group w-full h-full flex justify-center items-center"
-            >
-              <img
-                src={obj.src}
-                alt=""
-                className="opacity-0 group-hover:opacity-100 transition-opacity duration-200 ease absolute top-25 w-25"
-              />
-              <div
-                className="w-full text-center"
-                onClick={() => {
-                  setCount(index);
-                  setDisplayGrid(false);
-                  handleCursor(false);
-                }}
+    <div className="bio-title absolute top-0 inset-x-0 h-full w-full flex justify-center items-center pointer-events-none">
+      <div
+        className="w-full"
+        onMouseEnter={() => {
+          setDisplayGrid(true);
+          handleCursor(false);
+        }}
+        onMouseLeave={() => setDisplayGrid(false)}
+        onClick={() => setDisplayGrid(false)}
+      >
+        {!displayGrid && (
+          <div className="flex justify-between w-full px-10 p-5">
+            <div className="flex gap-10 pointer-events-auto">
+              <p
+                className={`mix-blend-difference text-white pointer-events-none transition-opacity duration-[400ms] ease-in-out flex gap-5 ${
+                  displayCursor ? "opacity-100" : "opacity-0"
+                }`}
               >
-                <LinkWrapper to={`/product/${obj.id}`}>
-                  <p className="cursor-pointer w-full p-5">{index + 1}</p>
-                </LinkWrapper>
-              </div>
+                <span>
+                  Issue <span className="italic">{artName}</span>
+                </span>
+                <span>by {artistName}</span>
+                <span>{year}</span>
+              </p>
             </div>
-          ))}
-        </div>
-      )}
+
+            <div className="pointer-events-auto">
+              <p
+                className={`mix-blend-difference text-white pointer-events-none transition-opacity duration-[400ms] ease-in-out flex gap-5 ${
+                  displayCursor ? "opacity-100" : "opacity-0"
+                }`}
+              >
+                <span>GRID</span>
+                <span>
+                  {artArray.length < 10 ? 0 : ""}
+                  {activeIndex + 1} - 0{artArray.length}
+                </span>
+              </p>
+            </div>
+          </div>
+        )}
+
+        {displayGrid && (
+          <div className="flex justify-between w-full px-10 pointer-events-auto">
+            {artArray.map((obj, index) => (
+              <div
+                key={index}
+                className="group w-full h-full flex justify-center items-center"
+              >
+                <img
+                  src={obj.src}
+                  alt=""
+                  className="opacity-0 group-hover:opacity-100 transition-opacity duration-200 ease absolute top-25 w-25"
+                />
+                <div className="w-full text-center">
+                  <LinkWrapper to={`/product/${obj.id}`}>
+                    <p className="cursor-pointer w-full p-5">{index + 1}</p>
+                  </LinkWrapper>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
 
 // Sorter that places bio into 2nd grid-container
 export const GridSetup = () => {
-  const handleCursor = useSetAtom(handleCustomCursor);
   const displayGrid = useAtomValue(handleDisplayGridAtom);
 
   // The active object based on index
   const data = useAtomValue(activeArtObj);
 
-  const debounceMouseMove = useMemo(
-    () =>
-      debounce(() => {
-        handleCursor(false);
-      }, 2000),
-    [],
-  );
-
-  const handleMouseMove = () => {
-    if (!displayGrid) {
-      handleCursor(true);
-      debounceMouseMove();
-    } else {
-      handleCursor(false);
-    }
-  };
-
   if (!data) return;
 
   return (
-    <div
-      className="relative"
-      onMouseMove={handleMouseMove}
-      onMouseLeave={handleMouseMove}
-    >
-      <section
-        className="relative w-full flex justify-center"
-        onClick={() => handleCursor(true)}
-      >
+    <div className="relative">
+      <section className="relative w-full flex justify-center">
         <div className="h-screen py-5">
           <img
             className={`h-full w-auto object-contain block ${displayGrid ? "opacity-10" : "opacity-100"} transition-opacity duration-200 ease`}
@@ -224,6 +171,24 @@ const Product = () => {
   // Fetches placeholder data and updated data, caches it as 'product'
   const data = useProductData(id);
   const handleCursor = useSetAtom(handleCustomCursor);
+  const displayGrid = useAtomValue(handleDisplayGridAtom);
+
+  const debounceMouseMove = useMemo(
+    () =>
+      debounce(() => {
+        handleCursor(false);
+      }, 2000),
+    [],
+  );
+
+  const handleMouseMove = () => {
+    if (!displayGrid) {
+      handleCursor(true);
+      debounceMouseMove();
+    } else {
+      handleCursor(false);
+    }
+  };
 
   const setCount = useSetAtom(setCountAtom);
   const setArtArray = useSetAtom(handleArtAtom);
@@ -244,14 +209,10 @@ const Product = () => {
   if (!data) return <div>Loading...</div>;
 
   return (
-    <main
-      className="relative"
-      onMouseLeave={() => handleCursor(false)}
-      onMouseEnter={() => handleCursor(true)}
-    >
+    <main className="relative">
       <Outlet />
 
-      <div className="cursor-none">
+      <div className="cursor-none" onMouseMove={handleMouseMove}>
         <div className="absolute inset-0 flex flex-col items-center justify-center pointer-events-auto">
           <DirectionContainers />
         </div>
